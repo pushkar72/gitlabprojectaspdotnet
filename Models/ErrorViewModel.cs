d3ba46ede8ecf7b8a2e38f99934b1424130ed406
Pushkar Jethwa <pushkarjethwa@outlook.com>
2020-12-15 16:09:18 +0530

    New Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitlabProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
